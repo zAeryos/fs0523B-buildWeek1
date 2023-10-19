544a47a614a21380d829ba7c2c2cb60ff48e2fc1
SimoD91 <simone0091@hotmail.com>
2023-10-19 15:08:13 +0200

    final-page provvisoria

---
+++ assets/js/finalPageScript.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+let input = document.querySelector(".checkbox");
+let button = document.getElementById("proceed2");
+let buttonGhost = document.querySelector(".buttontrasp")
+
+input.addEventListener("change", stateHandle);
+
+function stateHandle() {
+if (input.checked == false){
+    button.style.display = "none";
+    buttonGhost.style.display = "block";
+    buttonGhost.style.display = "visible";
+  } else{
+    button.style.display = "block";
+    button.style.visibility = "visible";
+    buttonGhost.style.display = "none";
+}
+}
+
+let containWelcome = document.querySelector(".container");
+let containQuestion = document.querySelector(".contenin2");
+
+button.addEventListener("click", changePage);
+
+function changePage() {
+  containWelcome.style.display = "none";
+  containQuestion.style.display = "block";
+  }
+
+});
+
