6b663a116ae6e6279972b682f94be45df6caa51e
zAeryos <dave_13_08@hotmail.it>
2023-10-20 09:49:20 +0200

    final page fix

---
+++ assets/js/results-page.js
@@ -0,0 +1,65 @@
+let risultato = "";
+let success = document.querySelector(".middle-section");
+let result1 = document.querySelector(".result1");
+let result2 = document.querySelector(".result2");
+let result3 = document.querySelector(".result3");
+
+/* prova % testo*/
+
+let testScore = 40;
+
+function testResults() {
+  if (testScore >= 60) {
+    result1.innerHTML = "Congratulations!";
+    result2.innerHTML = "You passed the exam.";
+    result2.classList.add("span_color");
+    result3.innerHTML =
+      "We'll send you the certificate in few minutes. Check your email (including promotions / spam folder)";
+  } else {
+    result1.innerHTML = "We're sorry,";
+    result2.innerHTML = "You failed the exam.";
+    result2.classList.add("span_color2");
+    result3.innerHTML =
+      "You'll be contacted by your professor to try and fix your grades shortly";
+  }
+}
+
+let domandeCorrette = 10;
+let domandeSbagliate = 20;
+
+const canvas = document.getElementById("my-chart");
+
+const charData = {
+  labels: ["Wrong", "Correct"],
+  data: [`${domandeSbagliate}`, `${domandeCorrette}`],
+};
+
+const myChart = document.getElementById("my-chart");
+
+console.log(Chart)
+new Chart(myChart, {
+  type: "doughnut",
+
+  data: {
+    labels: charData.labels,
+    datasets: [
+      {
+        label: "Risultati",
+        data: charData.data,
+        backgroundColor: ["#D20094", "#00FFFF"],
+      },
+    ],
+  },
+  options: {
+    borderWidth: 0,
+    borderRadius: 0,
+    cutout: 225,
+    radius: 225,
+
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
